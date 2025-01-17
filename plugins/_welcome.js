dbf6a439903b5ef77d72b775bf607bd367815b92
deylinqff <maycraftminecraft4@gmail.com>
2025-01-17 17:59:05 -0600

    Actualizar _welcome.js

--- plugins/_welcome.js
+++ plugins/_welcome.js
@@ -25,7 +25,7 @@ export async function before(m, { conn, participants, groupMetadata }) {
     let chat = global.db.data.chats[m.chat];
 
     if (chat.welcome && m.messageStubType === 27) {
-        let wel = `┌─⪩ Kakaroto-bot  🐉 \n│「 𝐁𝐈𝐄𝐍𝐕𝐄𝐍𝐈𝐃𝐎 😁 」\n└┬⪩ 「 @${userId.split`@`[0]} 」\n    │🐉  ${groupMetadata.subject}\n   └───────────────┈ ⳹\n\n> ✐ Puedes usar *#menu* para ver la lista de comandos.`;
+        let wel = `┌─⌬ 𝑲𝒊𝒓𝒊𝒕𝒐-𝑩𝒐𝒕 \n│「 𝐁𝐈𝐄𝐍𝐕𝐄𝐍𝐈𝐃𝐎 😁 」\n└┬⌬ 「 @${userId.split`@`[0]} 」\n    │🐉  ${groupMetadata.subject}\n   └───────────────┈ ⳹\n\n> ✐ Puedes usar *#menu* para ver la lista de comandos.`;
         try {
             await conn.sendMini(m.chat, packname, dev, wel, img, img, channel, fkontak);
         } catch (sendError) {
@@ -35,7 +35,7 @@ export async function before(m, { conn, participants, groupMetadata }) {
 
     // Mensaje de despedida (cuando se sale)
     if (chat.welcome && m.messageStubType === 28) {
-        let bye = `┌─⪩ Kakaroto-bot 🐉  \n│「 𝐀𝐃𝐈Ó𝐒 🗣️‼️ 」\n└┬⪩ 「 @${userId.split`@`[0]} 」\n   │😔 SE NOS FUE xd\n   │👺 NO VUELVAS SAIYAN\n   └───────────────┈ ⳹`;
+        let bye = `┌─⌬ 𝑲𝒊𝒓𝒊𝒕𝒐-𝑩𝒐𝒕  \n│「 𝐀𝐃𝐈Ó𝐒 🗣️‼️ 」\n└┬⌬ 「 @${userId.split`@`[0]} 」\n   │😔 SE NOS FUE xd\n   │👺 NO VUELVAS SAIYAN\n   └───────────────┈ ⳹`;
         let img2;
         try {
             img2 = await (await fetch(goodbyeImage)).buffer(); 
@@ -47,7 +47,7 @@ export async function before(m, { conn, participants, groupMetadata }) {
 
     // Mensaje de expulsión (cuando se echa a alguien)
     if (chat.welcome && m.messageStubType === 32) {
-        let kick = `┌─⪩ Kakaroto-bot  🐉 \n│「 𝐀𝐃𝐈Ó𝐒 🗣️‼️ 」\n└┬⪩ 「 @${userId.split`@`[0]} 」\n   │😔 SE NOS FUE xd\n   │👺 NO VUELVAS SAIYAN\n   └───────────────┈ ⳹`;
+        let kick = `┌─⌬ 𝑲𝒊𝒓𝒊𝒕𝒐-𝑩𝒐𝒕 \n│「 𝐀𝐃𝐈Ó𝐒 🗣️‼️ 」\n└┬⌬ 「 @${userId.split`@`[0]} 」\n   │😔 SE NOS FUE xd\n   │👺 Nunca te quisimos aqui\n   └───────────────┈ ⳹`;
         let img3;
         try {
             img3 = await (await fetch(goodbyeImage)).buffer();
