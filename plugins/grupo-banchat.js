10b7b863792d2b4eaf178ffc17d446494f666b2c
deylinqff <maycraftminecraft4@gmail.com>
2025-01-17 14:27:30 -0600

    Actualizar grupo-banchat.js

--- plugins/grupo-banchat.js
+++ plugins/grupo-banchat.js
@@ -1,7 +1,7 @@
 let handler = async (m) => {
 
 global.db.data.chats[m.chat].isBanned = true
-conn.reply(m.chat, `🍬 *Este chat fue baneado con éxito*`, m, rcanal)
+conn.reply(m.chat, `⚡ *Este chat fue baneado con éxito*`, m, rcanal)
 
 }
 handler.help = ['banchat']
