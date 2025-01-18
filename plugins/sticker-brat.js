617cf3b1622ae5c93a0931aca690542ee2626282
deylinqff <maycraftminecraft4@gmail.com>
2025-01-17 21:41:50 -0600

    Actualizar sticker-brat.js

--- plugins/sticker-brat.js
+++ plugins/sticker-brat.js
@@ -32,7 +32,7 @@ const handler = async (m, {
 }) => {
     if (!text) {
         return conn.sendMessage(m.chat, {
-            text: '🍬 Por favor ingresa el texto para hacer un sticker.',
+            text: '✎ Por favor ingresa el texto para hacer un sticker.',
         }, {
             quoted: m
         });
