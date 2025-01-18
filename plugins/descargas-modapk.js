f9ed693e5bf7e6bf2fb7888f25f985ed7f7f1de8
deylinqff <maycraftminecraft4@gmail.com>
2025-01-18 10:19:52 -0600

    Actualizar descargas-modapk.js

--- plugins/descargas-modapk.js
+++ plugins/descargas-modapk.js
@@ -1,10 +1,10 @@
 import { search, download } from 'aptoide-scraper'
 
 var handler = async (m, {conn, usedPrefix, command, text}) => {
-if (!text) return conn.reply(m.chat, '🍬 Por favor, ingrese el nombre de la apk para descargarlo.', m, rcanal)
+if (!text) return conn.reply(m.chat, '『 ✎ 』 Por favor, ingrese el nombre de la apk para descargarlo.', m, rcanal)
 try {
 await m.react(rwait)
-conn.reply(m.chat, '🍭 *Descargando su aplicación, espere un momento...', m, {
+conn.reply(m.chat, '『⇓⇓』 *Descargando su aplicación, espere un momento...', m, {
 contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, showAdAttribution: true,
 title: packname,
 body: dev,
@@ -14,13 +14,13 @@ let searchA = await search(text)
 let data5 = await download(searchA[0].id)
 let txt = `*乂  APTOIDE - DESCARGAS* 乂\n\n`
 txt += `☁️ *Nombre* : ${data5.name}\n`
-txt += `🍬 *Package* : ${data5.package}\n`
+txt += `🗂️ *Package* : ${data5.package}\n`
 txt += `🚩 *Update* : ${data5.lastup}\n`
 txt += `⚖ *Peso* :  ${data5.size}`
 await conn.sendFile(m.chat, data5.icon, 'thumbnail.jpg', txt, m, null, rcanal) 
 await m.react(done)  
 if (data5.size.includes('GB') || data5.size.replace(' MB', '') > 999) {
-return await conn.reply(m.chat, '🍭 El archivo es demaciado pesado.', m, rcanal )}
+return await conn.reply(m.chat, '『☒』 El archivo es demaciado pesado.', m, rcanal )}
 await conn.sendMessage(m.chat, {document: {url: data5.dllink}, mimetype: 'application/vnd.android.package-archive', fileName: data5.name + '.apk', caption: null}, {quoted: fkontak})
 } catch {
 return conn.reply(m.chat, '⚠️ Ocurrió un fallo...', m, rcanal )}}
