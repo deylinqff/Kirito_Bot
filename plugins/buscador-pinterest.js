c5714989703a03477dd2697043c57029d7346cc0
deylinqff <maycraftminecraft4@gmail.com>
2025-01-19 10:09:48 -0600

    Actualizar buscador-pinterest.js

--- plugins/buscador-pinterest.js
+++ plugins/buscador-pinterest.js
@@ -11,10 +11,10 @@ let handler = async (_0x10bd40, {
   command: _0x5ad406
 }) => {
   if (!_0x27db11) {
-    return _0x9c7141.reply(_0x10bd40.chat, "🍬 Por favor, ingresa lo que deseas buscar en pinterest.", _0x10bd40, rcanal);
+    return _0x9c7141.reply(_0x10bd40.chat, "『🔎』 Por favor, ingresa lo que deseas buscar en pinterest.", _0x10bd40, rcanal);
   }
     await _0x10bd40.react(rwait);
- _0x9c7141.reply(_0x10bd40.chat, '🍭 Descargando su imagen, espere un momento...', _0x10bd40, {
+ _0x9c7141.reply(_0x10bd40.chat, '『⇓⇓』 Descargando su imagen, espere un momento...', _0x10bd40, {
  contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, showAdAttribution: true,
  title: packname,
  body: dev,
@@ -62,7 +62,7 @@ let handler = async (_0x10bd40, {
       'nativeFlowMessage': proto.Message.InteractiveMessage.NativeFlowMessage.fromObject({
         'buttons': [{
           'name': "cta_url",
-          'buttonParamsJson': "{\"display_text\":\"url 🍬\",\"Url\":\"https://www.pinterest.com/search/pins/?rs=typed&q=" + _0x27db11 + "\",\"merchant_url\":\"https://www.pinterest.com/search/pins/?rs=typed&q=" + _0x27db11 + "\"}"
+          'buttonParamsJson': "{\"display_text\":\"url ✎\",\"Url\":\"https://www.pinterest.com/search/pins/?rs=typed&q=" + _0x27db11 + "\",\"merchant_url\":\"https://www.pinterest.com/search/pins/?rs=typed&q=" + _0x27db11 + "\"}"
         }]
       })
     });
@@ -76,7 +76,7 @@ let handler = async (_0x10bd40, {
         },
         'interactiveMessage': proto.Message.InteractiveMessage.fromObject({
           'body': proto.Message.InteractiveMessage.Body.create({
-            'text': "🍭 Resultado de: " + _0x27db11
+            'text': "〘⌬〙 Resultado de: " + _0x27db11
           }),
           'footer': proto.Message.InteractiveMessage.Footer.create({
             'text': "⪛✰ Pinterest - Busquedas ✰⪜"
