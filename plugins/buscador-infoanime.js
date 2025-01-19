384cff08128e9791167e60487c696ba9f4099457
deylinqff <maycraftminecraft4@gmail.com>
2025-01-19 10:07:36 -0600

    Actualizar buscador-infoanime.js

--- plugins/buscador-infoanime.js
+++ plugins/buscador-infoanime.js
@@ -2,7 +2,7 @@ import fetch from 'node-fetch'
 
 var handler = async (m, { conn, usedPrefix, command, text }) => {
 
-if (!text) return conn.reply(m.chat, `🍬 *Ingrese el nombre de algun anime*\n\nEjemplo, ${usedPrefix + command} Roshidere`, m, rcanal)
+if (!text) return conn.reply(m.chat, `『✎』 *Ingrese el nombre de algun anime*\n\nEjemplo, ${usedPrefix + command} Roshidere`, m, rcanal)
 let res = await fetch('https://api.jikan.moe/v4/manga?q=' + text)
 if (!res.ok) return conn.reply(m.chat, `⚠️ *Ocurrió un fallo*`, m, rcanal)
 
