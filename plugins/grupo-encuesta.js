d75633ad40f6706ed14fe1353bc832f93d242ad7
deylinqff <maycraftminecraft4@gmail.com>
2025-01-20 21:06:17 -0600

    Actualizar grupo-encuesta.js

--- plugins/grupo-encuesta.js
+++ plugins/grupo-encuesta.js
@@ -1,7 +1,7 @@
 let handler = async (m, { conn, text, args, usedPrefix, command }) => {
 
-if (!args[0]) throw `🍬️ Ingrese un texto para iniciar la escuesta.\n\n📌 Ejemplo : \n*${usedPrefix + command}* texto|texto2...`
-if (!text.includes('|')) throw  `🍭 Separe las encuestas con *|* \n\n📌 Ejemplo : \n*${usedPrefix + command}* texto|texto2...`
+if (!args[0]) throw `✎ Ingrese un texto para iniciar la escuesta.\n\n📌 Ejemplo : \n*${usedPrefix + command}* texto|texto2...`
+if (!text.includes('|')) throw  `♔︎ Separe las encuestas con *|* \n\n📌 Ejemplo : \n*${usedPrefix + command}* texto|texto2...`
 let a = []
 let b = text.split('|')
 for (let c = 0; c < b.length; c++) {
