0b56234c9ad02dfb5041787b3c906d8086cefb32
deylinqff <maycraftminecraft4@gmail.com>
2025-01-20 21:12:36 -0600

    Actualizar grupo-infogrupo.js

--- plugins/grupo-infogrupo.js
+++ plugins/grupo-infogrupo.js
@@ -5,35 +5,35 @@ const handler = async (m, {conn, participants, groupMetadata}) => {
   const listAdmin = groupAdmins.map((v, i) => `${i + 1}. @${v.id.split('@')[0]}`).join('\n');
   const owner = groupMetadata.owner || groupAdmins.find((p) => p.admin === 'superadmin')?.id || m.chat.split`-`[0] + '@s.whatsapp.net';
   const text = `*✧･ﾟ INFO GRUPO ﾟ･✧*
-❀ *ID:*
+✎ *ID:*
 → ${groupMetadata.id}
-⚘ *Nombre:*
+✎ *Nombre:*
 → ${groupMetadata.subject}
-✦ *Descripción:*
+✎ *Descripción:*
 → ${groupMetadata.desc?.toString() || 'Sin Descripción'}
-❖ *Miembros:*
+✎ *Miembros:*
 → ${participants.length} Participantes
-✰ *Creador del Grupo:*
+✎ *Creador del Grupo:*
 → @${owner.split('@')[0]}
-✥ *Administradores:*
+✎ *Administradores:*
 ${listAdmin}
 
-˚₊· ͟͟͞͞➳❥ *CONFIGURACIÓN*
+˚₊· ͟͟͞͞➙♛ *CONFIGURACIÓN*
 
-◈ *Welcome:* ${welcome ? '✅' : ''}
-◈ *Detect:* ${detect ? '✅' : '❌'}  
-◈ *Antilink:* ${antiLink ? '✅' : '❌'} 
-◈ *Autoaceptar:* ${autoAceptar ? '✅' : '❌'} 
-◈ *Autorechazar:* ${autoRechazar ? '✅' : '❌'} 
-◈ *Nfsw:* ${nsfw ? '✅' : '❌'} 
-◈ *Antiprivado:* ${antiPrivate ? '✅' : '❌'} 
-◈ *Modoadmin:* ${modoadmin ? '✅' : '❌'} 
-◈ *Antiver:* ${antiviewonce ? '✅' : '❌'} 
-◈ *Reacción* ${reaction ? "✅️" : "❌️"}
-◈ *Antispam:* ${antiSpam ? '✅' : '❌'} 
-◈ *Restrict:* ${restrict ? '✅' : '❌'} 
-◈ *Antitoxic:* ${antiToxic ? '✅' : '❌'} 
-◈ *Antitraba:* ${antiTraba ? '✅' : '❌'} 
+✰ *Welcome:* ${welcome ? '✅' : ''}
+✫ *Detect:* ${detect ? '✅' : '❌'}  
+✰ *Antilink:* ${antiLink ? '✅' : '❌'} 
+✫ *Autoaceptar:* ${autoAceptar ? '✅' : '❌'} 
+✰ *Autorechazar:* ${autoRechazar ? '✅' : '❌'} 
+✫ *Nfsw:* ${nsfw ? '✅' : '❌'} 
+✰ *Antiprivado:* ${antiPrivate ? '✅' : '❌'} 
+✫ *Modoadmin:* ${modoadmin ? '✅' : '❌'} 
+✰ *Antiver:* ${antiviewonce ? '✅' : '❌'} 
+✫ *Reacción* ${reaction ? "✅️" : "❌️"}
+✰ *Antispam:* ${antiSpam ? '✅' : '❌'} 
+✫ *Restrict:* ${restrict ? '✅' : '❌'} 
+✰ *Antitoxic:* ${antiToxic ? '✅' : '❌'} 
+✫ *Antitraba:* ${antiTraba ? '✅' : '❌'} 
 `.trim();
   conn.sendFile(m.chat, pp, 'img.jpg', text, m, false, {mentions: [...groupAdmins.map((v) => v.id), owner]});
 };
