a635b4c5ef64b22f93d0d908d4294c2192223fe5
deylinqff <maycraftminecraft4@gmail.com>
2025-01-21 11:25:50 -0600

    Actualizar info-reporte.js

--- plugins/info-reporte.js
+++ plugins/info-reporte.js
@@ -1,7 +1,7 @@
 let handler = async (m, { conn, text, usedPrefix, command }) => {
-    if (!text) return conn.reply(m.chat, '🍬 *Ingrese el error que desea reportar.*', m)
-    if (text.length < 10) return conn.reply(m.chat, '🍭 *Especifique bien el error, mínimo 10 caracteres.*', m)
-    if (text.length > 1000) return conn.reply(m.chat, '🍭 *Máximo 1000 caracteres para enviar el error.*', m)
+    if (!text) return conn.reply(m.chat, '✎ *Ingrese el error que desea reportar.*', m)
+    if (text.length < 10) return conn.reply(m.chat, '✎ *Especifique bien el error, mínimo 10 caracteres.*', m)
+    if (text.length > 1000) return conn.reply(m.chat, '✎ *Máximo 1000 caracteres para enviar el error.*', m)
     const teks = `*✖️ \`R E P O R T E\` ✖️*
 
 ☁️ Número:
