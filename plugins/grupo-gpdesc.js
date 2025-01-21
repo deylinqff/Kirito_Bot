4975661e0e84b043ab4875b0ba77c579a71ae744
deylinqff <maycraftminecraft4@gmail.com>
2025-01-20 21:09:13 -0600

    Actualizar grupo-gpdesc.js

--- plugins/grupo-gpdesc.js
+++ plugins/grupo-gpdesc.js
@@ -1,6 +1,6 @@
 const handler = async (m, {conn, args}) => {
   await conn.groupUpdateDescription(m.chat, `${args.join(' ')}`);
-  m.reply('*🍬 La descripción del grupo se modifico correctamente*');
+  m.reply('*👑 La descripción del grupo se modifico correctamente*');
 };
 handler.help = ['groupdesc <text>'];
 handler.tags = ['grupo'];
