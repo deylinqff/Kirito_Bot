98c12578864deb02879a4aff04b83d56c49adad8
deylinqff <maycraftminecraft4@gmail.com>
2025-01-21 23:27:13 -0600

    Actualizar rg-ser-premium.js

--- plugins/rg-ser-premium.js
+++ plugins/rg-ser-premium.js
@@ -1,16 +1,16 @@
 let handler = async (m, { conn, text }) => {
   let user = global.db.data.users[m.sender];
-  if (!text) return conn.reply(m.chat, '*🍬 Ingresa el tipo de membresía y la duración.*\n> Ejemplo: #comprarpremium 1 dia', m);
+  if (!text) return conn.reply(m.chat, '*👑 Ingresa el tipo de membresía y la duración.*\n> Ejemplo: #comprarpremium 1 dia', m);
 
   let [amount, unit] = text.split(' ');
   amount = parseInt(amount);
-  if (isNaN(amount)) return conn.reply(m.chat, '🍬 La cantidad debe ser un número.', m);
+  if (isNaN(amount)) return conn.reply(m.chat, '⚡ La cantidad debe ser un número.', m);
   
   const units = { minuto: 1, minutos: 1, hora: 60, horas: 60, dia: 1440, dias: 1440 };
-  if (!units[unit.toLowerCase()]) return conn.reply(m.chat, '🍬 Unidad de tiempo no válida. Usa minutos, horas o días.', m);
+  if (!units[unit.toLowerCase()]) return conn.reply(m.chat, '⚡ Unidad de tiempo no válida. Usa minutos, horas o días.', m);
 
   let cost = amount * (units[unit.toLowerCase()] / 200);
-  if (user.coin < cost) return conn.reply(m.chat, `😂 No tienes suficientes ${global.moneda}. Necesitas ${cost} ${global.moneda} para comprar esta membresía.`, m);
+  if (user.coin < cost) return conn.reply(m.chat, `⚡ No tienes suficientes ${global.moneda}. Necesitas ${cost} ${global.moneda} para comprar esta membresía.`, m);
 
   user.coin -= cost;
   user.premium = true;
