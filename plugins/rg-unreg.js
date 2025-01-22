000c24702dbdd324e779071a87354128f070bb50
deylinqff <maycraftminecraft4@gmail.com>
2025-01-21 23:29:59 -0600

    Actualizar rg-unreg.js

--- plugins/rg-unreg.js
+++ plugins/rg-unreg.js
@@ -3,7 +3,7 @@ let handler = async (m, { conn, text }) => {
 let user = global.db.data.users[m.sender]
 
 user.registered = false
-return conn.reply(m.chat, `*『✅』 Usted Ya No Está En Mi Base De Datos*`, m, rcanal)
+return conn.reply(m.chat, `*『🚀』 Usted Ya No Está En Mi Base De Datos*`, m, rcanal)
 
 }
 handler.help = ['unreg']
