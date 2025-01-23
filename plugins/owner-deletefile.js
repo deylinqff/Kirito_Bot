0ee61b29cc44f3402df289a0b37ce698d0768d63
deylinqff <maycraftminecraft4@gmail.com>
2025-01-22 21:55:25 -0600

    Actualizar owner-deletefile.js

--- plugins/owner-deletefile.js
+++ plugins/owner-deletefile.js
@@ -10,10 +10,10 @@ import {
 } from 'fs'
 
 let handler = async (m, { conn, usedPrefix: _p, __dirname, args, text }) => {
-    if (!text) return conn.reply(m.chat, `🍬 Ingresa la ruta y el nombre del archivo que deseas eliminar.`, m, rcanal)
+    if (!text) return conn.reply(m.chat, `⚡ Ingresa la ruta y el nombre del archivo que deseas eliminar.`, m, rcanal)
     
     const file = text.trim()
-    if (!existsSync(file)) return conn.reply(m.chat, `🍭 Archivo no encontrado.`, m, rcanal)
+    if (!existsSync(file)) return conn.reply(m.chat, `👑 Archivo no encontrado.`, m, rcanal)
     
     unlinkSync(file)
     conn.reply(m.chat, `✅ El archivo *${file}* ha sido eliminado con éxito.`, m, rcanal)
