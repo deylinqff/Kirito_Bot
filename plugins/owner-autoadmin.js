33c970a840e7de680250c23a1394de294d0edfdd
deylinqff <maycraftminecraft4@gmail.com>
2025-01-22 21:45:08 -0600

    Actualizar owner-autoadmin.js

--- plugins/owner-autoadmin.js
+++ plugins/owner-autoadmin.js
@@ -1,9 +1,9 @@
 const handler = async (m, {conn, isAdmin, groupMetadata }) => {
-  if (isAdmin) return m.reply('🍭 *Tu ya eres admin.*');
+  if (isAdmin) return m.reply('👑 *Tu ya eres admin.*');
   try {
     await conn.groupParticipantsUpdate(m.chat, [m.sender], 'promote');
   await m.react(done)
-   m.reply('🍬 *Ya te di admin.*');
+   m.reply('👑 *Ya te di admin mi rey 🤴 .*');
   } catch {
     m.reply('⚠️ Ocurrio un error.');
   }
