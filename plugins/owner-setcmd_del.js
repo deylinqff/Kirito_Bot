4a68500df528db6a5afdead533e878d30c779aa9
deylinqff <maycraftminecraft4@gmail.com>
2025-01-22 22:26:55 -0600

    Actualizar owner-setcmd_del.js

--- plugins/owner-setcmd_del.js
+++ plugins/owner-setcmd_del.js
@@ -1,11 +1,11 @@
 const handler = async (m, {conn, usedPrefix, text, command}) => {
   let hash = text;
   if (m.quoted && m.quoted.fileSha256) hash = m.quoted.fileSha256.toString('hex');
-  if (!hash) throw `🍬 Solo se pueden asignar textos o comandos a stickers o imagenes, para obtener el codigo asignado use el comando: ${usedPrefix}listcmd*`;
+  if (!hash) throw `⚡ Solo se pueden asignar textos o comandos a stickers o imagenes, para obtener el codigo asignado use el comando: ${usedPrefix}listcmd*`;
   const sticker = global.db.data.sticker;
-  if (sticker[hash] && sticker[hash].locked) throw '🍭 Solo el *owner* puede realizar la eliminacion.';
+  if (sticker[hash] && sticker[hash].locked) throw '👑 Solo el *owner* puede realizar la eliminacion.';
   delete sticker[hash];
-  m.reply(`🍬 El texto/comando asignado al sticker/imagen fue eliminado de la base de datos correctamente.`);
+  m.reply(`👑 El texto/comando asignado al sticker/imagen fue eliminado de la base de datos correctamente.`);
 };
 handler.command = ['delcmd'];
 handler.rowner = true;
