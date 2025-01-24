f6e220c3c7c7c1238a3f13c4fb161471e459bd1c
deylinqff <maycraftminecraft4@gmail.com>
2025-01-24 14:23:07 -0600

    Actualizar buscador-tiktoksearch.js

--- plugins/buscador-tiktoksearch.js
+++ plugins/buscador-tiktoksearch.js
@@ -45,7 +45,7 @@ deviceListMetadataVersion: 2
 },
 interactiveMessage: proto.Message.InteractiveMessage.fromObject({
 body: proto.Message.InteractiveMessage.Body.create({ text: '『 ➥ 』 Resultado de: ' + text }),
-footer: proto.Message.InteractiveMessage.Footer.create({ text: '⪛✰ Tiktok - Busquedas ✰⪜' }),
+footer: proto.Message.InteractiveMessage.Footer.create({ text: '✬𝐊𝐢𝐫𝐢𝐭𝐨 - 𝐁𝐨𝐭✭' }),
 header: proto.Message.InteractiveMessage.Header.create({ hasMediaAttachment: false }),
 carouselMessage: proto.Message.InteractiveMessage.CarouselMessage.fromObject({ cards: [...results] })})}}
 }, { quoted: message })
