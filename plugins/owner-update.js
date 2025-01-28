1cd5780a2aba9feac8474984788fce2c9c43925b
deylinqff <maycraftminecraft4@gmail.com>
2025-01-28 12:23:00 -0600

    Actualizar owner-update.js

--- plugins/owner-update.js
+++ plugins/owner-update.js
@@ -8,9 +8,9 @@ try {
 const stdout = execSync('git pull' + (m.fromMe && text ? ' ' + text : ''));
 let messager = stdout.toString()
 
-if (messager.includes('☘️ Ya estoy actualizada.')) messager = '☘️ Ya estoy actualizada a la última versión.'
+if (messager.includes('⚡ Ya estoy actualizada.')) messager = '⚡ Ya estoy actualizada a la última versión.'
 
-if (messager.includes('💫 Actualizando.')) messager = '✨️ Procesando, espere un momento mientras me actualizo.\n\n' + stdout.toString()
+if (messager.includes('👑 Actualizando.')) messager = '⚡ Procesando, espere un momento mientras me actualizo.\n\n' + stdout.toString()
 conn.reply(m.chat, messager, m, rcanal,)
 
 } catch { 
@@ -20,12 +20,12 @@ const status = execSync('git status --porcelain')
 
 if (status.length > 0) {
 const conflictedFiles = status.toString().split('\n').filter(line => line.trim() !== '').map(line => {
-if (line.includes('.npm/') || line.includes('.cache/') || line.includes('tmp/') || line.includes('YoshiSession/') || line.includes('npm-debug.log')) {
+if (line.includes('.npm/') || line.includes('.cache/') || line.includes('tmp/') || line.includes('YukiSession/') || line.includes('npm-debug.log')) {
 return null
 }
 return '*→ ' + line.slice(3) + '*'}).filter(Boolean)
 if (conflictedFiles.length > 0) {
-const errorMessage = `💭 Se han hecho cambios locales qué entran en conflicto con las Actualizaciones del Repositorio, Para actualizar, reinstala el Bot o realiza las actualizaciones manualmente.\n\n✰ *ARCHIVOS EN CONFLICTO*\n\n${conflictedFiles.join('\n')}`
+const errorMessage = `⚡ Se han hecho cambios locales qué entran en conflicto con las Actualizaciones del Repositorio, Para actualizar, reinstala el Bot o realiza las actualizaciones manualmente.\n\n✰ *ARCHIVOS EN CONFLICTO*\n\n${conflictedFiles.join('\n')}`
 await conn.reply(m.chat, errorMessage, m, rcanal,)
 }
 }
