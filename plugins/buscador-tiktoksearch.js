04564440d4181fd87ddf93dd2ebf347a0df9e7b5
deylinqff <maycraftminecraft4@gmail.com>
2025-02-01 13:39:04 -0600

    Actualizar buscador-tiktoksearch.js

--- plugins/buscador-tiktoksearch.js
+++ plugins/buscador-tiktoksearch.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 const {proto, generateWAMessageFromContent, prepareWAMessageMedia, generateWAMessageContent, getDevice} = (await import("@whiskeysockets/baileys")).default
 
 let handler = async (message, { conn, text, usedPrefix, command }) => {
-if (!text) return conn.reply(message.chat, '『 🔎 』 Por favor, ingrese lo que desea buscar en tiktok.', message, rcanal)
+if (!text) return conn.reply(message.chat, '🍬 Por favor, ingrese lo que desea buscar en tiktok.', message, rcanal)
 async function createVideoMessage(url) {
 const { videoMessage } = await generateWAMessageContent({ video: { url } }, { upload: conn.waUploadToServer })
 return videoMessage
@@ -15,7 +15,7 @@ const j = Math.floor(Math.random() * (i + 1));
 }
 try {
 await message.react(rwait)
-conn.reply(message.chat, '『⇓⇓』 Descargando Su Video, espere un momento...', message, {
+conn.reply(message.chat, '『 🔎 』 Descargando Su Video, espere un momento...', message, {
 contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, showAdAttribution: true,
 title: packname,
 body: dev,
@@ -45,7 +45,7 @@ deviceListMetadataVersion: 2
 },
 interactiveMessage: proto.Message.InteractiveMessage.fromObject({
 body: proto.Message.InteractiveMessage.Body.create({ text: '『 ➥ 』 Resultado de: ' + text }),
-footer: proto.Message.InteractiveMessage.Footer.create({ text: '✬𝐊𝐢𝐫𝐢𝐭𝐨 - 𝐁𝐨𝐭✭' }),
+footer: proto.Message.InteractiveMessage.Footer.create({ text: '★᭄ꦿ᭄ꦿ𝐊𝐢𝐫𝐢𝐭𝐨-𝐁𝐨𝐭' }),
 header: proto.Message.InteractiveMessage.Header.create({ hasMediaAttachment: false }),
 carouselMessage: proto.Message.InteractiveMessage.CarouselMessage.fromObject({ cards: [...results] })})}}
 }, { quoted: message })
