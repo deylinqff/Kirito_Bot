45fff68ad1c7bdba048d57f6d77f8457ce04a546
deylinqff <maycraftminecraft4@gmail.com>
2025-02-01 19:34:31 -0600

    Actualizar plugins

---
+++ plugins/_balidcomads.js
@@ -0,0 +1,31 @@
+export async function before(m) {
+  if (!m.text || !global.prefix.test(m.text)) {
+    return;
+  }
+
+  const usedPrefix = global.prefix.exec(m.text)[0];
+  const command = m.text.slice(usedPrefix.length).trim().split(' ')[0].toLowerCase();
+
+  const validCommand = (command, plugins) => {
+    for (let plugin of Object.values(plugins)) {
+     if (plugin.command && (Array.isArray(plugin.command) ? plugin.command : [plugin.command]).includes(command)) {
+        return true;
+      }
+    }
+    return false;
+  };
+
+  if (validCommand(command, global.plugins)) {
+    let chat = global.db.data.chats[m.chat];
+    let user = global.db.data.users[m.sender];
+    if (chat.isBanned) return;
+    if (!user.commands) {
+      user.commands = 0;
+    }
+    user.commands += 1;
+   // await conn.sendPresenceUpdate('composing', m.chat);
+  } else {
+   const comando = m.text.trim().split(' ')[0];
+   await m.reply(`〘 ☣ 〙 𝑬𝒍 𝒄𝒐𝒎𝒂𝒏𝒅𝒐 *${comando}* 𝒏𝒐 𝒆𝒙𝒊𝒔𝒕𝒆.\n𝑷𝒂𝒓𝒂 𝒗𝒆𝒓 𝒍𝒂 𝒍𝒊𝒔𝒕𝒂 𝒅𝒆 𝒄𝒐𝒎𝒂𝒏𝒅𝒐𝒔 𝒖𝒔𝒂:\n✎ *#menu*`);
+  }
+}
