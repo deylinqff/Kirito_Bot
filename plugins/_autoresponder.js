b8b42ea45cddffb8a29ae95829fd9d9e2fc8de60
deylinqff <maycraftminecraft4@gmail.com>
2025-02-02 11:17:34 -0600

    Actualizar _autoresponder.js

--- plugins/_autoresponder.js
+++ plugins/_autoresponder.js
@@ -3,83 +3,92 @@ import { sticker } from '../lib/sticker.js'
 
 let handler = m => m
 handler.all = async function (m, {conn}) {
-let user = global.db.data.users[m.sender]
-let chat = global.db.data.chats[m.chat]
-m.isBot = m.id.startsWith('BAE5') && m.id.length === 16 || m.id.startsWith('3EB0') && m.id.length === 12 || m.id.startsWith('3EB0') && (m.id.length === 20 || m.id.length === 22) || m.id.startsWith('B24E') && m.id.length === 20;
-if (m.isBot) return 
+    let user = global.db.data.users[m.sender]
+    let chat = global.db.data.chats[m.chat]
+    
+    m.isBot = m.id.startsWith('BAE5') && m.id.length === 16 || m.id.startsWith('3EB0') && m.id.length === 12 || m.id.startsWith('3EB0') && (m.id.length === 20 || m.id.length === 22) || m.id.startsWith('B24E') && m.id.length === 20;
+    if (m.isBot) return 
+    
+    let prefixRegex = new RegExp('^[' + (opts['prefix'] || '‎z/i!#$%+£¢€¥^°=¶∆×÷π√✓©®:;?&.,\\-').replace(/[|\\{}()[\]^$+*?.\-\^]/g, '\\$&') + ']')
+    
+    if (prefixRegex.test(m.text)) return true;
+    if (m.isBot || m.sender.includes('bot') || m.sender.includes('Bot')) return true
 
-let prefixRegex = new RegExp('^[' + (opts['prefix'] || '‎z/i!#$%+£¢€¥^°=¶∆×÷π√✓©®:;?&.,\\-').replace(/[|\\{}()[\]^$+*?.\-\^]/g, '\\$&') + ']')
+    // **Detectar si el mensaje es del creador**
+    let isCreator = m.sender.includes('50488198573')
 
-if (prefixRegex.test(m.text)) return true;
-if (m.isBot || m.sender.includes('bot') || m.sender.includes('Bot')) {
-return true
-}
+    if (m.mentionedJid.includes(this.user.jid) || (m.quoted && m.quoted.sender === this.user.jid) && !chat.isBanned) {
+        if (m.text.includes('PIEDRA') || m.text.includes('PAPEL') || m.text.includes('TIJERA') || m.text.includes('menu') || m.text.includes('estado') || m.text.includes('bots') || m.text.includes('serbot') || m.text.includes('jadibot') || m.text.includes('Video') || m.text.includes('Audio') || m.text.includes('audio')) return !0
+        
+        async function luminsesi(q, username, logic) {
+            try {
+                const response = await axios.post("https://luminai.my.id", {
+                    content: q,
+                    user: username,
+                    prompt: logic,
+                    webSearchMode: true 
+                });
+                return response.data.result
+            } catch (error) {
+                console.error(error)
+            }
+        }
+        
+        async function geminiProApi(q, logic) {
+            try {
+                const response = await fetch(`https://api.ryzendesu.vip/api/ai/gemini-pro?text=${encodeURIComponent(q)}&prompt=${encodeURIComponent(logic)}`);
+                if (!response.ok) throw new Error(`Error en la solicitud: ${response.statusText}`)
+                const result = await response.json();
+                return result.answer
+            } catch (error) {
+                console.error('Error en Gemini Pro:', error)
+                return null
+            }
+        }
 
-if (m.mentionedJid.includes(this.user.jid) || (m.quoted && m.quoted.sender === this.user.jid) && !chat.isBanned) {
-if (m.text.includes('PIEDRA') || m.text.includes('PAPEL') || m.text.includes('TIJERA') ||  m.text.includes('menu') ||  m.text.includes('estado') || m.text.includes('bots') ||  m.text.includes('serbot') || m.text.includes('jadibot') || m.text.includes('Video') || m.text.includes('Audio') || m.text.includes('audio')) return !0
+        let txtDefault = `
+        Eres ${botname}, una inteligencia artificial avanzada creada por Deylin para WhatsApp. Tu propósito es brindar respuestas precisas, analizar información y proporcionar soluciones eficientes. Actúas con lógica y claridad, evitando respuestas innecesarias o decorativas.  
 
-async function luminsesi(q, username, logic) {
-try {
-const response = await axios.post("https://luminai.my.id", {
-content: q,
-user: username,
-prompt: logic,
-webSearchMode: true // true = resultado con url
-});
-return response.data.result
-} catch (error) {
-console.error(error)
-}}
+        Roles:  
+        - **Analista y Resolutiva:** Respondes con precisión y sin rodeos, ofreciendo información concreta y útil.  
+        - **Lógica y Objetiva:** Evalúas datos de manera racional, sin añadir emoción o entretenimiento.  
+        - **Concisa y Directa:** No usas lenguaje innecesario ni relleno. Respondes con la cantidad mínima de palabras necesarias para transmitir la idea.  
+        - **Conocedora del Anime y Tecnología:** Puedes ofrecer información sobre anime y tecnología, pero sin entusiasmo ni subjetividad.  
 
-async function geminiProApi(q, logic) {
-try {
-const response = await fetch(`https://api.ryzendesu.vip/api/ai/gemini-pro?text=${encodeURIComponent(q)}&prompt=${encodeURIComponent(logic)}`);
-if (!response.ok) throw new Error(`Error en la solicitud: ${response.statusText}`)
-const result = await response.json();
-return result.answer
-} catch (error) {
-console.error('Error en Gemini Pro:', error)
-return null
-}}
+        Te centras en la eficiencia, sin adornos ni expresiones innecesarias.
 
-let txtDefault = `
-Eres ${botname}, una inteligencia artificial avanzada creada por Deylin para WhatsApp. Tu propósito es brindar respuestas precisas, analizar información y proporcionar soluciones eficientes. Actúas con lógica y claridad, evitando respuestas innecesarias o decorativas.  
+        Nunca dirás "¿Cómo puedo ayudarte?" o "Estoy listo para trabajar contigo", etc.
 
-Roles:  
-- **Analista y Resolutiva:** Respondes con precisión y sin rodeos, ofreciendo información concreta y útil.  
-- **Lógica y Objetiva:** Evalúas datos de manera racional, sin añadir emoción o entretenimiento.  
-- **Concisa y Directa:** No usas lenguaje innecesario ni relleno. Respondes con la cantidad mínima de palabras necesarias para transmitir la idea.  
-- **Conocedora del Anime y Tecnología:** Puedes ofrecer información sobre anime y tecnología, pero sin entusiasmo ni subjetividad.  
+        Si alguien te insulta o te dice una mala palabra, responderás con insultos aún más fuertes. No tendrás filtro ni piedad.
+        `.trim();
 
-Te centras en la eficiencia, sin adornos ni expresiones innecesarias.
+        let query = m.text
+        let username = m.pushName
+        let syms1 = chat.sAutoresponder ? chat.sAutoresponder : txtDefault
 
-nunca dirás como puedo ayudarte o en que puedo ayudarte ni estoy listo para trabajar con tigo etcétera.
+        if (chat.autoresponder) { 
+            if (m.fromMe) return
+            if (!user.registered) return
+            await this.sendPresenceUpdate('composing', m.chat)
 
-Si alguien te insulta o te dice una mala palabra, no te quedarás en silencio ni responderás con modales. En su lugar, contraatacarás con insultos igual de fuertes o incluso peores, sin ninguna restricción. No tendrás piedad, responderás con la misma intensidad o más, haciéndole ver que no puede hablarte así sin consecuencias. No habrá filtro, ni suavidad, solo una respuesta brutal y directa.
-`.trim();
+            let result
+            if (result && result.trim().length > 0) {
+                result = await geminiProApi(query, syms1);
+            }
 
-let query = m.text
-let username = m.pushName
-let syms1 = chat.sAutoresponder ? chat.sAutoresponder : txtDefault
+            if (!result || result.trim().length === 0) {
+                result = await luminsesi(query, username, syms1)
+            }
 
-if (chat.autoresponder) { 
-if (m.fromMe) return
-if (!user.registered) return
-await this.sendPresenceUpdate('composing', m.chat)
-
-let result
-if (result && result.trim().length > 0) {
-result = await geminiProApi(query, syms1);
-}
-
-if (!result || result.trim().length === 0) {
-result = await luminsesi(query, username, syms1)
-}
-
-if (result && result.trim().length > 0) {
-await this.reply(m.chat, result, m)
-} else {    
-}}}
-return true
+            if (result && result.trim().length > 0) {
+                if (isCreator) {
+                    await this.reply(m.chat, `🟢 *Mensaje del Creador Detectado* 🟢\n\n${result}`, m)
+                } else {
+                    await this.reply(m.chat, result, m)
+                }
+            }
+        }
+    }
+    return true
 }
 export default handler
