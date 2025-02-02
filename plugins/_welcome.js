ee1042dd491fd8519f7948c02b6887c0edf42f6f
deylinqff <maycraftminecraft4@gmail.com>
2025-02-01 18:26:57 -0600

    Actualizar _welcome.js

--- plugins/_welcome.js
+++ plugins/_welcome.js
@@ -13,12 +13,12 @@ export async function before(m, { conn, participants, groupMetadata }) {
 
   if (chat.welcome) {
     if (m.messageStubType === WAMessageStubType.GROUP_PARTICIPANT_ADD) {
-      let bienvenida = `👑 *¡Bienvenido a ${groupMetadata.subject}!* ⚡\n\n ${taguser}, disfruta tu estadía en el grupo.\n ${global.welcom1}\n\n⚡ Usa *#help* para ver los comandos disponibles.\n https://chat.whatsapp.com/B8cm2kI4MRIIXhlRl7ierQ`
+      let bienvenida = `👑 *¡Bienvenido a ${groupMetadata.subject}!* ⚡\n\n ${taguser}, disfruta tu estadía en el grupo.\n ${global.welcom1}\n\n> Usa *#help* para ver los comandos disponibles.\n https://chat.whatsapp.com/H9Er7VDTtCSGSvGZEUqPVb`
       let img = await (await fetch(welcomeImage)).buffer()
       await conn.sendMessage(m.chat, { image: img, caption: bienvenida, mentions: [who] })
       
     } else if (m.messageStubType === WAMessageStubType.GROUP_PARTICIPANT_REMOVE || m.messageStubType === WAMessageStubType.GROUP_PARTICIPANT_LEAVE) {
-      let bye = `⚡ *${taguser} ha salido de ${groupMetadata.subject}.*\n\n${global.welcom2}\n\n👻 ¡Esperamos verte de nuevo!\n https://chat.whatsapp.com/B8cm2kI4MRIIXhlRl7ierQ`
+      let bye = `⚡ *${taguser} ha salido de ${groupMetadata.subject}.*\n\n${global.welcom2}\n\n👻 ¡Esperamos verte de nuevo!\n https://chat.whatsapp.com/H9Er7VDTtCSGSvGZEUqPVb`
       let img = await (await fetch(goodbyeImage)).buffer()
       await conn.sendMessage(m.chat, { image: img, caption: bye, mentions: [who] })
     }
