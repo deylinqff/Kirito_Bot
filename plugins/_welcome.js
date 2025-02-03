bd5e627474fc300c64ef9afac111fc1cd9b1dc1e
deylinqff <maycraftminecraft4@gmail.com>
2025-02-03 11:56:19 -0600

    Actualizar _welcome.js

--- plugins/_welcome.js
+++ plugins/_welcome.js
@@ -19,7 +19,18 @@ export async function before(m, { conn, participants, groupMetadata }) {
     }
 
     if (m.messageStubType === WAMessageStubType.GROUP_PARTICIPANT_ADD) {
-      let bienvenida = `👑 *¡Bienvenido a ${groupMetadata.subject}!* \n\n${taguser}, disfruta tu estadía en el grupo.\n\n Ya somos〘${participants.length}〙 Miembros.\n\n ${global.welcom1}\n\n> Usa *#help* para ver los comandos disponibles.\n https://chat.whatsapp.com/H9Er7VDTtCSGSvGZEUqPVb`
+      let bienvenida = ╭━━━━━━━━━━━━━━━━━✦\n +
+                 │ ⚔️ *¡BIENVENIDO, ESPADACHÍN!* ⚔️\n +
+                 │ 🏰 *Reino:* 『${groupMetadata.subject}』\n +
+                 │ 👤 *Guerrero:* ${taguser}\n +
+                 │ ⚡ *Fuerza del Gremio:* ${participants.length} miembros\n +
+                 ╰━━━━━━━━━━━━━━━━━✦\n\n +
+                 🖤 *"El mundo no es justo, pero tú decides cómo luchar."* - Kirito\n\n +
+                 💠 ${global.welcom1}\n\n +
+                 🗡️ *Usa:* #help para conocer tus habilidades.\n +
+                 🔗 *Únete a la batalla:* https://chat.whatsapp.com/H9Er7VDTtCSGSvGZEUqPVb`
+
+await conn.sendMessage(m.chat, { image: img, caption: bienvenida, mentions: [who] });
       await conn.sendMessage(m.chat, { image: img, caption: bienvenida, mentions: [who] })
     } else if (m.messageStubType === WAMessageStubType.GROUP_PARTICIPANT_REMOVE || m.messageStubType === WAMessageStubType.GROUP_PARTICIPANT_LEAVE) {
       let bye = `⚡ *${taguser} ha salido de ${groupMetadata.subject}.*\n\n Sólo quedamos〘 ${participants.length} 〙Miembros.\n\n${global.welcom2}\n\n👻 ¡Esperamos verte de nuevo!\n https://chat.whatsapp.com/H9Er7VDTtCSGSvGZEUqPVb`
