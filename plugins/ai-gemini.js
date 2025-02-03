0b7e05230b06d2fea10e294e0bd9cbbc59453335
deylinqff <maycraftminecraft4@gmail.com>
2025-02-02 23:00:00 -0600

    Actualizar ai-gemini.js

--- plugins/ai-gemini.js
+++ plugins/ai-gemini.js
@@ -1,6 +1,6 @@
 import fetch from 'node-fetch'
 var handler = async (m, { text,  usedPrefix, command }) => {
-if (!text) return conn.reply(m.chat, `✎ Ingrese una petición para que Gemini lo responda.`, m, rcanal)
+if (!text) return conn.reply(m.chat, `『👑』𝙄𝙣𝙜𝙧𝙚𝙨𝙚 𝙪𝙣 𝙩𝙚𝙭𝙩𝙤 𝙥𝙖𝙧𝙖 𝙪𝙨𝙖𝙧 𝙚𝙨𝙩𝙚 𝙘𝙤𝙢𝙖𝙣𝙙𝙤.\n\n• 𝙋𝙤𝙧 𝙚𝙟𝙚𝙢𝙥𝙡𝙤:\n${usedPrefix + command} Hola`, m, rcanal)
 try {
 await m.react(rwait)
 conn.sendPresenceUpdate('composing', m.chat)
@@ -8,8 +8,8 @@ var apii = await fetch(`https://apis-starlights-team.koyeb.app/starlight/gemini?
 var res = await apii.json()
 await m.reply(res.result)
 } catch {
-await m.react('❌')
-await conn.reply(m.chat, `⚠️ Gemini no puede responder a esa pregunta.`, m, rcanal)
+await m.react(error)
+await conn.reply(m.chat, `『⚙️』𝙊𝙘𝙪𝙧𝙧𝙞𝙤 𝙪𝙣 𝙚𝙧𝙧𝙤𝙧 𝙚𝙣 𝙚𝙡 𝙘𝙤𝙢𝙖𝙣𝙙𝙤, 𝙧𝙚𝙥𝙤𝙧𝙩𝙖𝙡𝙤 𝙖𝙡 𝙘𝙧𝙚𝙖𝙙𝙤𝙧 𝙙𝙚𝙡 𝙗𝙤𝙩.`, m, rcanal)
 }}
 handler.command = ['gemini']
 handler.help = ['gemini']
