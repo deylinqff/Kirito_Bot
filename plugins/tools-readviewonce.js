1ba2e3b229d2fe280372a0ac1e3b9119f2afe976
deylinqff <maycraftminecraft4@gmail.com>
2025-02-03 21:48:51 -0600

    Actualizar tools-readviewonce.js

--- plugins/tools-readviewonce.js
+++ plugins/tools-readviewonce.js
@@ -1,36 +1,17 @@
-//Este si es para el whatsapp actualizado💁🏻‍♂️
+let { downloadContentFromMessage } = (await import('@whiskeysockets/baileys'));
 
-const { downloadContentFromMessage } = require('@whiskeysockets/baileys');
-
-const handler = async (m, { conn }) => {
-  if (!m.quoted) return conn.reply(m.chat, 'Responde a una imagen ViewOnce.', m)
-  if (m.quoted.mtype !== 'viewOnceMessage') return conn.reply(m.chat, 'Responde a una imagen ViewOnce.', m)
-
-  const msg = m.quoted.message
-  const type = Object.keys(msg)[0]
-  const media = await downloadContentFromMessage(msg[type], type === 'imageMessage' ? 'image' : 'video')
-
-  if (!media) return conn.reply(m.chat, 'Error al descargar el archivo.', m)
-
-  const buffer = Buffer.from([])
-  for await (const chunk of media) {
-    buffer = Buffer.concat([buffer, chunk])
-  }
-
-  switch (type) {
-    case 'imageMessage':
-      return conn.sendFile(m.chat, buffer, 'media.jpg', msg[type].caption || '', m)
-    case 'videoMessage':
-      return conn.sendFile(m.chat, buffer, 'media.mp4', msg[type].caption || '', m)
-    default:
-      return conn.reply(m.chat, 'Tipo de archivo no soportado.', m)
-  }
-}
-
+let handler = async (m, { conn }) => {
+if (!m.quoted) return conn.reply(m.chat, `👑 Responde a una imagen ViewOnce.`, m)
+if (!m?.quoted || !m?.quoted?.viewOnce) return conn.reply(m.chat, `👑 Responde a una imagen ViewOnce.`, m)
+let buffer = await m.quoted.download(false);
+if (/videoMessage/.test(m.quoted.mtype)) {
+return conn.sendFile(m.chat, buffer, 'media.mp4', m.quoted.caption || '', m)
+} else if (/imageMessage/.test(m.quoted.mtype)) {
+return conn.sendFile(m.chat, buffer, 'media.jpg', m.quoted?.caption || '', m)
+}}
 handler.help = ['ver']
 handler.tags = ['tools']
-handler.command = ['readviewonce', 'read', 'ver', 'readvo']
-handler.limit = 1
-handler.register = true
+handler.command = ['readviewonce', 'read', 'readvo'] 
+handler.register = true 
 
-module.exports = handler
+export default handler
