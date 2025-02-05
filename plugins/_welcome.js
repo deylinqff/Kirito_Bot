c987b82ad38e2d922c09285bb82dc9c3903c8cab
deylinqff <maycraftminecraft4@gmail.com>
2025-02-04 19:23:00 -0600

    Actualizar _welcome.js

--- plugins/_welcome.js
+++ plugins/_welcome.js
@@ -21,7 +21,11 @@ export async function before(m, { conn, participants, groupMetadata }) {
         let bienvenida = `👑 𝐇𝐨𝐥𝐚, ${taguser} ¡𝐁𝐢𝐞𝐧𝐯𝐞𝐧𝐢𝐝𝐨/𝐀 \n ${groupMetadata.subject} \n 𝐃𝐢𝐬𝐟𝐫𝐮𝐭𝐚 𝐭𝐮 𝐞𝐬𝐭𝐚𝐝𝐢𝐚 𝐞𝐧 𝐞𝐥 𝐠𝐫𝐮𝐩𝐨. \n\n> 𝑼𝒔𝒂 *#help* 𝒑𝒂𝒓𝒂 𝒗𝒆𝒓 𝒍𝒐𝒔 𝒄𝒐𝒎𝒂𝒏𝒅𝒐𝒔 𝒅𝒊𝒔𝒑𝒐𝒏𝒊𝒃𝒍𝒆𝒔.\n https://chat.whatsapp.com/H9Er7VDTtCSGSvGZEUqPVb`
       await conn.sendMessage(m.chat, { image: img, caption: bienvenida, mentions: [who] })
     } else if (m.messageStubType === WAMessageStubType.GROUP_PARTICIPANT_REMOVE || m.messageStubType === WAMessageStubType.GROUP_PARTICIPANT_LEAVE) {
-        let bye = `⚡ 𝐁𝐚𝐲 *${taguser} 𝐒𝐞 𝐟𝐮𝐞 𝐝𝐞\n ${groupMetadata.subject}.*\n👻 ¡𝐄𝐬𝐩𝐞𝐫𝐚𝐦𝐨𝐬 𝐧𝐨 𝐯𝐞𝐫𝐭𝐞 𝐧𝐮𝐧𝐜𝐚!\n https://chat.whatsapp.com/H9Er7VDTtCSGSvGZEUqPVb`
+        let bye = `✭~- 𝐊𝐈𝐑𝐈𝐓𝐎 𝐁𝐎𝐓 -~✭\n\n
+ 𝐁𝐚𝐲 *${taguser} 𝐒𝐞 𝐟𝐮𝐞 𝐝𝐞\n ${groupMetadata.subject}.*\n
+__°—👻 ¡𝐄𝐬𝐩𝐞𝐫𝐚𝐦𝐨𝐬 𝐧𝐨 𝐯𝐞𝐫𝐭𝐞 𝐧𝐮𝐧𝐜𝐚! 👻— ~_°
+
+_°       ➳𝐂𝐫𝐞𝐚𝐭𝐨𝐫 𝐛𝐲 𝓓𝓮𝔂𝓵𝓲𝓷 ...`
       await conn.sendMessage(m.chat, { image: img, caption: bye, mentions: [who] })
     }
   }
