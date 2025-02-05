10d37e578e0da337de3d29090a8f7fee95bef22c
deylinqff <maycraftminecraft4@gmail.com>
2025-02-04 22:31:37 -0600

    Actualizar info-creador.js

--- plugins/info-creador.js
+++ plugins/info-creador.js
@@ -26,9 +26,7 @@ let handler = async (m, { conn, usedPrefix, text, args, command }) => {
 
   // Llamar a la función para enviar la información de contacto
   await sendContactArray(conn, m.chat, [
-    [`${nomorown}`, `👑 Propietario`, `🚀 𝑫𝒆𝒚𝒍𝒊𝒏`, dev, 'Deylibaqudano40@gmail.com', ` 𝑯𝒐𝒏𝒅𝒖𝒓𝒂𝒔 🇭🇳`, `https://github.com/deylinqff`, bio],
-    [`${conn.user.jid.split('@')[0]}`, `Es Un Bot ⚡`, `${packname}`, `📵 No Hacer Spam`, 'moisesmusic04@gmail.com', `🇨🇴 Colombia`, `https://github.com/deylinqff/Kirito_Bot`, biobot]
-  ], m)
+    [`${nomorown}`, `👑 Propietario`, `🚀 𝑫𝒆𝒚𝒍𝒊𝒏`, dev, 'Deylibaqudano40@gmail.com', ` 𝑯𝒐𝒏𝒅𝒖𝒓𝒂𝒔 🇭🇳`, `https://github.com/deylinqff`, bio], m)
 }
 
 handler.help = ["creador", "owner"]
