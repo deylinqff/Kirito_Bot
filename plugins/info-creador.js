023ff958b43b3e6a1f9ffa6216b8a1e75623bbbe
deylinqff <maycraftminecraft4@gmail.com>
2025-02-04 23:11:48 -0600

    Actualizar info-creador.js

--- plugins/info-creador.js
+++ plugins/info-creador.js
@@ -1,11 +1,20 @@
-function handler(m) {
-    let ownerNumber = '50488198573'; // Número del creador
+import PhoneNumber from 'awesome-phonenumber';
+
+async function handler(m, { conn }) {
+    let ownerNumber = '50488198573'; // Número del creador (sin @s.whatsapp.net)
     let ownerJid = `${ownerNumber}@s.whatsapp.net`;
+
     let name = '👑 Deylin'; // Nombre del creador
     let packname = 'Kirito Bot'; // Nombre del bot
     let dev = 'By Deylin'; // Desarrollador
-    let channel = 'https://whatsapp.com/channel/0029VawF8fBBvvsktcInIz3m'; // URL (puede ser de GitHub, YouTube, etc.)
-    let banner = 'https://files.catbox.moe/vtsk5v.jpg'; // URL de la imagen grande
+    let channel = 'https://github.com/deylinqff/Kirito_Bot'; // URL
+    let banner = 'https://files.catbox.moe/tm6axp.jpg'; // Imagen
+    let bio = (await conn.fetchStatus(ownerJid).catch(_ => 'Sin Biografía')).status || 'Sin Biografía';
+    let ppUrl = await conn.profilePictureUrl(ownerJid).catch(_ => banner);
+
+    await sendContactArray(conn, m.chat, [
+        [`${ownerNumber}`, `👑 Propietario`, packname, dev, 'darel0954@gmail.com', `🇭🇳 Honduras`, channel, bio]
+    ], m, { thumbnail: ppUrl });
 
     conn.sendContact(m.chat, [[ownerJid, name]], m, {
         contextInfo: { 
@@ -27,8 +36,51 @@ function handler(m) {
     });
 }
 
-handler.help = ['owner'];
+handler.help = ['owner', 'creador'];
 handler.tags = ['info'];
-handler.command = ['owner', 'creador', 'creator', 'dueño'];
+handler.command = /^(owner|creador|creator|dueño)$/i;
 
 export default handler;
+
+async function sendContactArray(conn, jid, data, quoted, options) {
+    if (!Array.isArray(data[0]) && typeof data[0] === 'string') data = [data];
+
+    let contacts = [];
+    for (let [number, name, title, role, email, region, website, bio] of data) {
+        number = number.replace(/[^0-9]/g, '');
+        let njid = number + '@s.whatsapp.net';
+
+        let vcard = `
+BEGIN:VCARD
+VERSION:3.0
+N:Sy;${name.replace(/\n/g, '\\n')};;;
+FN:${name.replace(/\n/g, '\\n')}
+item.ORG:${title}
+item1.TEL;waid=${number}:${PhoneNumber('+' + number).getNumber('international')}
+item1.X-ABLabel:${role}
+item2.EMAIL;type=INTERNET:${email}
+item2.X-ABLabel:📧 Email
+item3.ADR:;;${region};;;;
+item3.X-ABADR:ac
+item3.X-ABLabel:🏷 Región
+item4.URL:${website}
+item4.X-ABLabel:Website
+item5.X-ABLabel:${bio}
+END:VCARD`.trim();
+
+        contacts.push({ vcard, displayName: name });
+    }
+
+    return await conn.sendMessage(jid, {
+        contacts: {
+            displayName: contacts.length > 1 ? `${contacts.length} contactos` : contacts[0].displayName,
+            contacts,
+        }
+    },
+    {
+        quoted,
+        ...options
+    });
+}
+
+// Código creado por Deyin
