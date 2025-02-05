a08ab8de95f5373fae0d7e74177b85ef41db7f24
deylinqff <maycraftminecraft4@gmail.com>
2025-02-04 23:31:06 -0600

    Actualizar info-creador.js

--- plugins/info-creador.js
+++ plugins/info-creador.js
@@ -1,31 +1,31 @@
 import PhoneNumber from 'awesome-phonenumber';
 
-async function handler(m, { conn }) {
-    let ownerNumber = '50488198573'; // Número del creador
+let handler = async (m, { conn }) => {
+    let ownerNumber = '50488198573';
     let ownerJid = `${ownerNumber}@s.whatsapp.net`;
 
-    let name = '👑 Deylin'; // Nombre del creador
+    let name = '👑 Deylin';
 
     let vcard = `
 BEGIN:VCARD
 VERSION:3.0
 N:Sy;${name};;;
 FN:${name}
-TEL;waid=${ownerNumber}:${PhoneNumber('+' + ownerNumber).getNumber('international')}
+TEL;waid=${ownerNumber}:${new PhoneNumber('+' + ownerNumber).getNumber('international')}
 END:VCARD`.trim();
 
-    conn.sendMessage(m.chat, {
+    await conn.sendMessage(m.chat, {
         contacts: {
             displayName: name,
             contacts: [{ vcard, displayName: name }]
         }
     }, { quoted: m });
-}
+};
 
 handler.help = ['owner', 'creador'];
 handler.tags = ['info'];
 handler.command = /^(owner|creador|creator|dueño)$/i;
 
-export default handler;
+export default handler; // Si tu bot usa CommonJS, cambia esto a module.exports = handler;
 
 // Código creado por Deyin
