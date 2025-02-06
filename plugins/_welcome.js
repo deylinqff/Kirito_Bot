55e3dd6fc9923a001e6782a5386b52c31f6ec591
deylinqff <maycraftminecraft4@gmail.com>
2025-02-06 08:52:34 -0600

    Actualizar _welcome.js

--- plugins/_welcome.js
+++ plugins/_welcome.js
@@ -7,7 +7,7 @@ export async function before(m, { conn, participants, groupMetadata }) {
   let who = m.messageStubParameters[0]
   let taguser = `@${who.split('@')[0]}`
   let chat = global.db.data.chats[m.chat]
-  let defaultImage = 'https://files.catbox.moe/uc4yvl.jpg';
+  let defaultImage = 'https://files.catbox.moe/56el7x.jpg';
 
   if (chat.welcome) {
     let img;
