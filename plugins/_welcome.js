b153fb1ad8cd2d13ce2d21bcd0db8abf77a1df3d
deylinqff <maycraftminecraft4@gmail.com>
2025-02-06 12:59:24 -0600

    Actualizar _welcome.js

--- plugins/_welcome.js
+++ plugins/_welcome.js
@@ -1,6 +1,5 @@
 import { WAMessageStubType } from '@whiskeysockets/baileys'
 import fetch from 'node-fetch'
-import Jimp from 'jimp'
 
 export async function before(m, { conn, participants, groupMetadata }) {
   if (!m.messageStubType || !m.isGroup) return true
@@ -8,25 +7,16 @@ export async function before(m, { conn, participants, groupMetadata }) {
   let who = m.messageStubParameters[0]
   let taguser = `@${who.split('@')[0]}`
   let chat = global.db.data.chats[m.chat]
-  let defaultImage = 'https://files.catbox.moe/56el7x.jpg'
-  let botname = 'Kirito Bot'
-  let creator = 'Deylin'
+  let defaultImage = 'https://files.catbox.moe/56el7x.jpg';
 
   if (chat.welcome) {
     let img;
     try {
-      let pp = await conn.profilePictureUrl(who, 'image')
-      img = await (await fetch(pp)).buffer()
+      let pp = await conn.profilePictureUrl(who, 'image');
+      img = await (await fetch(pp)).buffer();
     } catch {
-      img = await (await fetch(defaultImage)).buffer()
+      img = await (await fetch(defaultImage)).buffer();
     }
-
-    // Cargar imagen y agregar marca de agua
-    let image = await Jimp.read(img)
-    let font = await Jimp.loadFont(Jimp.FONT_SANS_32_WHITE)
-    image.print(font, 10, 10, `${botname} - Creado por ${creator}`)
-    
-    let finalImage = await image.getBufferAsync(Jimp.MIME_JPEG)
 
     if (m.messageStubType === WAMessageStubType.GROUP_PARTICIPANT_ADD) {
       let bienvenida = `╔═══════⩽✰⩾═══════╗
@@ -34,10 +24,10 @@ export async function before(m, { conn, participants, groupMetadata }) {
 ╠═══════⩽✰⩾═══════╝
 ║╭──────────────┄
 ║│ *user* : ${taguser} 
-║│ *Grupo* : ${groupMetadata.subject}
+║│ *Gastado* : ${groupMetadata.subject}
 ║╰──────────────┄
 ╚═══════⩽✰⩾═══════╝`
-      await conn.sendAi(m.chat, botname, bienvenida, finalImage, finalImage, canal)
+      await conn.sendMessage(m.chat, { image: img, caption: bienvenida, mentions: [who] })
     } else if (m.messageStubType === WAMessageStubType.GROUP_PARTICIPANT_REMOVE || m.messageStubType === WAMessageStubType.GROUP_PARTICIPANT_LEAVE) {
       let bye = `╔═══════⩽✰⩾═══════╗
 ║               𝐁𝐀𝐘
@@ -47,7 +37,7 @@ export async function before(m, { conn, participants, groupMetadata }) {
 ║│ *Grupo* : ${groupMetadata.subject}
 ║╰──────────────┄
 ╚═══════⩽✰⩾═══════╝`
-      await conn.sendAi(m.chat, botname, bye, finalImage, finalImage, canal)
+      await conn.sendMessage(m.chat, { image: img, caption: bye, mentions: [who] })
     }
   }
 
