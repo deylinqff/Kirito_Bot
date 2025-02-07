622ba5424a9ef55fb4ea04ef40e0fe0517df977b
deylinqff <maycraftminecraft4@gmail.com>
2025-02-07 17:10:45 -0600

    Actualizar info-creador.js

--- plugins/info-creador.js
+++ plugins/info-creador.js
@@ -1,11 +1,7 @@
-import PhoneNumber from 'awesome-phonenumber';
-
-async function handler(m, { conn }) {
-    let ownerNumber = '50488198573';
-    let ownerJid = `${ownerNumber}@s.whatsapp.net`;
-
-    let name = '${name}';
-
+function handler(m) {
+let name = conn.getName(`${numcreador}@s.whatsapp.net`)
+let ownerN = `${numcreador}`
+conn.sendContact(m.chat, [[`${ownerN}@s.whatsapp.net`, `${name}`]], m, {
     let vcard = `
 BEGIN:VCARD
 VERSION:3.0
