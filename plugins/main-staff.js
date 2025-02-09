8f8d4e12e86b89093193ba66017c094faa04696c
deylinqff <maycraftminecraft4@gmail.com>
2025-02-09 14:27:01 -0600

    Actualizar main-staff.js

--- plugins/main-staff.js
+++ plugins/main-staff.js
@@ -1,32 +1,5 @@
 let handler = async (m, { conn, command, usedPrefix }) => {
-let staff = `╔═❖ EQUIPO DE AYUDANTES ❖═╗
-╠ 📌 Dueño: ${creador}
-╠ 🤖 Bot: ${botname}
-╠ ⚜️ Versión: ${vs}
-╠ 📚 Librería: ${libreria} ${baileys}
-╚═════════════════════╝
-
-🌟 𝗖𝗥𝗘𝗔𝗗𝗢𝗥
-━━━━━━━━━━━━━━━
-👑 *𝑫𝒆𝒚𝒍𝒊𝒏*
-⚡ *Rol:* Creador
-🌟*Number:* wa.me/+50488198573
-👾 *GitHub:* https://github.com/deylinqff
-
-💠 𝗖𝗢𝗟𝗔𝗕𝗢𝗥𝗔𝗗𝗢𝗥𝗘𝗦
-━━━━━━━━━━━━━━━
-🪄 *Davidius⁩*
-🔖 *Rol:* Developer
-🌟 *Number:* wa.me/+595975677765
-
-🪄 *Jose*
-🔖 *Rol:* Developer
-🌟 *Number:* wa.me/+5217641291269
-
-
-━━━━━━━━━━━━━━━
-🔥¡Gracias por apoyar el proyecto!🔥
-`
+let staff = `no disponible`
 await conn.sendFile(m.chat, icons, 'yaemori.jpg', staff.trim(), fkontak, true, {
 contextInfo: {
 'forwardingScore': 200,
