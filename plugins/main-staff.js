704f7cc33577bb25dad47ecff39a7006c0f5517a
deylinqff <maycraftminecraft4@gmail.com>
2025-02-08 23:40:45 -0600

    Actualizar main-staff.js

--- plugins/main-staff.js
+++ plugins/main-staff.js
@@ -1,52 +1,36 @@
 let handler = async (m, { conn, command, usedPrefix }) => {
 let img = './src/catalogo.jpg'
-let staff = `✨ *EQUIPO DE AYUDANTES* ✨
-👑 *Dueño* ${creador}
-🍬 *Bot:* ${botname}
-⚜️ *Versión:* ${vs}
-📚 *Libreria:* ${libreria} ${baileys}
+let staff = `╔═❖ *EQUIPO DE AYUDANTES* ❖═╗
+╠ 📌 *Dueño:* ${creador}
+╠ 🤖 *Bot:* ${botname}
+╠ ⚜️ *Versión:* ${vs}
+╠ 📚 *Librería:* ${libreria} ${baileys}
+╚══════════════════════╝
 
-🪐 *Creador:*
+🌟 *𝗖𝗥𝗘𝗔𝗗𝗢𝗥*
+━━━━━━━━━━━━━━━
+👑 *𝑫𝒆𝒚𝒍𝒊𝒏*
+⚡ *Rol:* Creador
+🌟 *Número:* wa.me/+50488198573
+👾 *GitHub:* https://github.com/deylinqff
 
-☁️ ⁱᵃᵐ|𝔇ĕ𝐬†𝓻⊙γ𒆜
-🔖 *Rol:* Creador
-👾 *GitHub:* https://github.com/The-King-Destroy
-
-🍃 *Colaboradores:*
-
-🫧 𝓔𝓶𝓶𝓪 𝓥𝓲𝓸𝓵𝓮𝓽𝓼 𝓥𝓮𝓻𝓼𝓲ó𝓷 
+💠 *𝗖𝗢𝗟𝗔𝗕𝗢𝗥𝗔𝗗𝗢𝗥𝗘𝗦*
+━━━━━━━━━━━━━━━
+🪄 *Davidius⁩*
 🔖 *Rol:* Developer
-👾 *GitHub:* https://github.com/Elpapiema
+🌟 *Número:* wa.me/+595975677765
 
-🍍 Niño Piña
+🪄 *Jose*
 🔖 *Rol:* Developer
-👾 *GitHub:* https://github.com/WillZek
+🌟 *Número:* wa.me/+5217641291269
 
-⚡ ☆꧁༒ĹєǤ𝒆𝐧𝐃༒꧂☆
-🔖 *Rol:* Developer
-👾 *GitHub:* https://github.com/Diomar-s
-
-☘️ I'm Fz' (Tesis)
-🔖 *Rol:* Developer
-👾 *GitHub:* https://github.com/FzTeis
-
-🌪️ 𝓛𝓮𝓰𝓷𝓪
-🔖 *Rol:* Moderador 
-👾 *GitHub:* https://github.com/Legna-chan
+━━━━━━━━━━━━━━━
+🔥 ¡Gracias por apoyar el proyecto! 🔥
 `
-await conn.sendFile(m.chat, img, 'yuki.jpg', staff.trim(), fkontak, true, {
+await conn.sendFile(m.chat, img, 'equipo.jpg', staff.trim(), fkontak, true, {
 contextInfo: {
 'forwardingScore': 200,
 'isForwarded': false,
-/*externalAdReply: {
-showAdAttribution: true,
-renderLargerThumbnail: false,
-title: packname,
-body: dev,
-mediaType: 1,
-sourceUrl: channel,
-thumbnailUrl: icono
-}}*/
 }
 }, { mentions: m.sender })
 m.react(emoji)
