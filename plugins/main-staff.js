2c14cfa84eec3406eb6dbbc5692517ff03644eec
deylinqff <maycraftminecraft4@gmail.com>
2025-02-08 23:48:28 -0600

    Actualizar main-staff.js

--- plugins/main-staff.js
+++ plugins/main-staff.js
@@ -1,44 +1,49 @@
-let handler = async (m, { conn }) => {
-    let img = './src/catalogo.jpg'
-
-    let staff = `╔═❖ *EQUIPO DE AYUDANTES* ❖═╗
-╠ 📌 *Dueño:* ${global.creador}
-╠ 🤖 *Bot:* ${global.botname}
-╠ ⚜️ *Versión:* ${global.vs}
-╠ 📚 *Librería:* ${global.libreria} ${global.baileys}
+let handler = async (m, { conn, command, usedPrefix }) => {
+let staff = `╔═❖ EQUIPO DE AYUDANTES ❖═╗
+╠ 📌 Dueño: ${creador}
+╠ 🤖 Bot: ${botname}
+╠ ⚜️ Versión: ${vs}
+╠ 📚 Librería: ${libreria} ${baileys}
 ╚══════════════════════╝
 
-🌟 *𝗖𝗥𝗘𝗔𝗗𝗢𝗥*
+🌟 𝗖𝗥𝗘𝗔𝗗𝗢𝗥
 ━━━━━━━━━━━━━━━
 👑 *𝑫𝒆𝒚𝒍𝒊𝒏*
 ⚡ *Rol:* Creador
-🌟 *Número:* wa.me/+50488198573
+🌟*Number:* wa.me/+50488198573
 👾 *GitHub:* https://github.com/deylinqff
 
-💠 *𝗖𝗢𝗟𝗔𝗕𝗢𝗥𝗔𝗗𝗢𝗥𝗘𝗦*
+💠 𝗖𝗢𝗟𝗔𝗕𝗢𝗥𝗔𝗗𝗢𝗥𝗘𝗦
 ━━━━━━━━━━━━━━━
 🪄 *Davidius⁩*
 🔖 *Rol:* Developer
-🌟 *Número:* wa.me/+595975677765
+🌟 *Number:* wa.me/+595975677765
 
 🪄 *Jose*
 🔖 *Rol:* Developer
-🌟 *Número:* wa.me/+5217641291269
+🌟 *Number:* wa.me/+5217641291269
+
 
 ━━━━━━━━━━━━━━━
-🔥 ¡Gracias por apoyar el proyecto! 🔥
+🔥 ¡Gracias por apoyar el proyecto! 🔥`
 `
+await conn.sendFile(m.chat, icons, 'yaemori.jpg', staff.trim(), fkontak, true, {
+contextInfo: {
+'forwardingScore': 200,
+'isForwarded': false,
+externalAdReply: {
+showAdAttribution: true,
+renderLargerThumbnail: false,
+title: `🥷 Developers 👑`,
+body: `✨ Staff Oficial`,
+mediaType: 1,
+sourceUrl: redes,
+thumbnailUrl: icono
+}}
+}, { mentions: m.sender })
+m.react(emoji)
 
-    await conn.sendFile(m.chat, img, 'equipo.jpg', staff.trim(), m, {
-        contextInfo: {
-            forwardingScore: 200,
-            isForwarded: false,
-        }
-    }, { mentions: [m.sender] })
-
-    await m.react('✅')
 }
-
 handler.help = ['staff']
 handler.command = ['colaboradores', 'staff']
 handler.register = true
