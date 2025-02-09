b86197f9466b9ed4e925cb8036c2c5c05ae26a30
deylinqff <maycraftminecraft4@gmail.com>
2025-02-09 00:01:25 -0600

    Actualizar owner-dsowner.js

--- plugins/owner-dsowner.js
+++ plugins/owner-dsowner.js
@@ -42,7 +42,7 @@ await conn.reply(m.chat, '⚙️ *Ocurrió un fallo*',  m, rcanal, )
 }
 handler.help = ['dsowner']
 handler.tags = ['owner']
-handler.command = ['delai', 'delyuki', 'dsowner', 'clearallsession']
+handler.command = ['delai', 'delkirito', 'dsowner', 'clearallsession']
 
 handler.rowner = true
 
