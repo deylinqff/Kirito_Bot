701017c90f9751f50ec979df239eb467fc45cbcc
deylinqff <maycraftminecraft4@gmail.com>
2025-02-11 17:44:02 -0600

    Actualizar config.js

--- config.js
+++ config.js
@@ -18,6 +18,7 @@ global.confirmCode = ''
 
 global.owner = [
   [ '50488198573', 'Deylin', true ],
+  [ '595975677765', 'Davidius', true ],
   [ '5217641291269', 'José', true ]
 ]; // <-- Corchete de cierre y coma añadidos
 
