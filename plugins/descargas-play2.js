98903e1770a39fc25fc969417e6a6f4d1a13dcda
deylinqff <maycraftminecraft4@gmail.com>
2025-02-11 19:26:36 -0600

    Actualizar descargas-play2.js

--- plugins/descargas-play2.js
+++ plugins/descargas-play2.js
@@ -8,7 +8,7 @@ const formatVideo = ['360', '480', '720', '1080', '1440', '4k'];
 const ddownr = {
   download: async (url, format) => {
     if (!formatAudio.includes(format) && !formatVideo.includes(format)) {
-      throw new Error('👑↛ 𝐹𝑜𝑟𝑚𝑎𝑡𝑜 𝑛𝑜 𝑠𝑜𝑝𝑜𝑟𝑡𝑎𝑏𝑙𝑒, 𝑣𝑒𝑟𝑖𝑓𝑖𝑐𝑎 𝑙𝑎 𝑙𝑖𝑠𝑡𝑎 𝑑𝑒 𝑓𝑜𝑟𝑚𝑎𝑡𝑜𝑠 𝑑𝑖𝑠𝑝𝑜𝑛𝑖𝑏𝑙𝑒𝑠_°');
+      throw new Error('Formato no soportado, verifica la lista de formatos disponibles.');
     }
 
     const config = {
@@ -26,7 +26,7 @@ const ddownr = {
         const downloadUrl = await ddownr.cekProgress(id);
         return downloadUrl;
       } else {
-        throw new Error('⚡↛ 𝐹𝑎𝑙𝑙𝑜 𝑎𝑙 𝑜𝑏𝑡𝑒𝑛𝑒𝑟 𝑙𝑜𝑠 𝑑𝑒𝑡𝑎𝑙𝑙𝑒𝑠 𝑑𝑒𝑙 𝑣𝑖𝑑𝑒𝑜_°');
+        throw new Error('Fallo al obtener los detalles del video.');
       }
     } catch (error) {
       console.error('Error:', error);
@@ -60,18 +60,18 @@ const ddownr = {
 const handler = async (m, { conn, text, usedPrefix, command }) => {
   try {
     if (!text.trim()) {
-      return conn.reply(m.chat, `✨↛ 𝐼𝑛𝑔𝑟𝑒𝑠𝑎 𝑒𝑙 𝑛𝑜𝑚𝑏𝑟𝑒 𝑑𝑒 𝑙𝑎 𝑚𝑢𝑠𝑖𝑐𝑎 𝑎 𝑑𝑒𝑠𝑐𝑎𝑟𝑔𝑎𝑟_°`, m);
+      return conn.reply(m.chat, `${emoji} ingresa el nombre de la música a descargar.`, m);
     }
 
     const search = await yts(text);
     if (!search.all || search.all.length === 0) {
-      return m.reply('✖️↛ 𝑁𝑜 𝑠𝑒 𝑒𝑛𝑐𝑜𝑛𝑡𝑟𝑎𝑟𝑜𝑛 𝑟𝑒𝑠𝑢𝑙𝑡𝑎𝑑𝑜𝑠 𝑝𝑎𝑟𝑎 𝑡𝑢 𝑏𝑢𝑠𝑞𝑢𝑒𝑑𝑎_°');
+      return m.reply('No se encontraron resultados para tu búsqueda.');
     }
 
     const videoInfo = search.all[0];
     const { title, thumbnail, timestamp, views, ago, url } = videoInfo;
     const vistas = formatViews(views);
-    const infoMessage = `🏓 𝑇𝑖𝑡𝑢𝑙𝑜: *${title}*\n*°.⎯⃘̶⎯̸⎯ܴ⎯̶᳞͇ࠝ⎯⃘̶⎯̸⎯ܴ⎯̶᳞͇ࠝ⎯⃘̶⎯̸.°*\n> 🚀↛ 𝐷𝑢𝑟𝑎𝑐𝑖𝑜𝑛: *${timestamp}*\n*°.⎯⃘̶⎯̸⎯ܴ⎯̶᳞͇ࠝ⎯⃘̶⎯̸⎯ܴ⎯̶᳞͇ࠝ⎯⃘̶⎯̸.°*\n> 👑↛ 𝑉𝑖𝑠𝑡𝑎𝑠: *${vistas}*\n*°.⎯⃘̶⎯̸⎯ܴ⎶᳞͇ࠝ⎯⃘̶⎯̸⎯ܴ⎯̶᳞͇ࠝ⎯⃘̶⎯̸.°*\n> ✨↛ 𝐶𝑎𝑛𝑎𝑙: *${videoInfo.author.name || 'Desconocido'}*\n*°.⎯⃘̶⎯̸⎯ܴ⎯̶᳞͇ࠝ⎯⃘̶⎯̸⎯ܴ⎯̶᳞͇ࠝ⎯⃘̶⎯̸.°*\n> 🪄↛ 𝑃𝑢𝑏𝑙𝑖𝑐𝑎𝑑𝑜: *${ago}*\n*°.⎯⃘̶⎯̸⎯ܴ⎶᳞͇ࠝ⎯⃘̶⎯̸⎯ܴ⎯̶᳞͇ࠝ⎯⃘̶⎯̸.°*\n> ⚡↛ 𝐸𝑛𝑙𝑎𝑐𝑒: ${url}`;
+    const infoMessage = `🎬 Título: *${title}*\n*°.⎯⃘̶⎯̸⎯ܴ⎯̶᳞͇ࠝ⎯⃘̶⎯̸⎯ܴ⎯̶᳞͇ࠝ⎯⃘̶⎯̸.°*\n> 🕒 Duración: *${timestamp}*\n*°.⎯⃘̶⎯̸⎯ܴ⎯̶᳞͇ࠝ⎯⃘̶⎯̸⎯ܴ⎯̶᳞͇ࠝ⎯⃘̶⎯̸.°*\n> 👀 Vistas: *${vistas}*\n*°.⎯⃘̶⎯̸⎯ܴ⎶᳞͇ࠝ⎯⃘̶⎯̸⎯ܴ⎯̶᳞͇ࠝ⎯⃘̶⎯̸.°*\n> 🔖 Canal: *${videoInfo.author.name || 'Desconocido'}*\n*°.⎯⃘̶⎯̸⎯ܴ⎯̶᳞͇ࠝ⎯⃘̶⎯̸⎯ܴ⎯̶᳞͇ࠝ⎯⃘̶⎯̸.°*\n> 📆 Publicado: *${ago}*\n*°.⎯⃘̶⎯̸⎯ܴ⎶᳞͇ࠝ⎯⃘̶⎯̸⎯ܴ⎯̶᳞͇ࠝ⎯⃘̶⎯̸.°*\n> 🔗 Enlace: ${url}`;
     const thumb = (await conn.getFile(thumbnail))?.data;
 
     const JT = {
@@ -127,21 +127,21 @@ const handler = async (m, { conn, text, usedPrefix, command }) => {
             document: { url: validUrl },
             fileName: `${title}.mp4`,
             mimetype: 'video/mp4',
-            caption: `🌷↛ 𝐴𝑞𝑢𝑖 𝑡𝑖𝑒𝑛𝑒𝑠 ｡^‿^｡`,
+            caption: `${emoji} Aqui tienes ฅ^•ﻌ•^ฅ.`,
             thumbnail: thumb
           }, { quoted: m });
         } else {
-          return m.reply(`🌷↛ *𝑁𝑜 𝑠𝑒 𝑝𝑢𝑑𝑜 𝑑𝑒𝑠𝑐𝑎𝑒𝑔𝑎𝑟 𝑒𝑙 𝑣𝑖𝑑𝑒𝑜:* 𝑁𝑜 𝑠𝑒 𝑒𝑛𝑐𝑜𝑛𝑡𝑟𝑜 𝑢𝑛 𝑒𝑛𝑘𝑎𝑐𝑒 𝑑𝑒 𝑑𝑒𝑠𝑐𝑎𝑟𝑔𝑎 𝑣𝑎𝑙𝑖𝑑𝑜_°`);
+          return m.reply(`${emoji2} *No se pudo descargar el video:* No se encontró un enlace de descarga válido.`);
         }
       } catch (error) {
-        console.error('✖️ *𝐸𝑅𝑅𝑂𝑅, 𝑎𝑙 𝑜𝑏𝑡𝑒𝑛𝑒𝑟 𝑙𝑎𝑠 𝑈𝑅𝐿 𝑑𝑒 𝑑𝑒𝑠𝑐𝑎𝑟𝑔𝑎_°:', error);
-        return m.reply(`✖️ *𝐸𝑅𝑅𝑂𝑅, 𝑎𝑙 𝑖𝑛𝑡𝑒𝑛𝑡𝑎𝑟 𝑑𝑒𝑠𝑐𝑎𝑒𝑔𝑎𝑟 𝑒𝑙 𝑣𝑖𝑑𝑒𝑜_°:* ${error.message}`);
+        console.error('Error al obtener las URL de descarga:', error);
+        return m.reply(`${msm} Error al intentar descargar el video: ${error.message}`);
       }
     } else {
-      throw "🌷↛ 𝐶𝑜𝑚𝑎𝑛𝑑𝑜 𝑛𝑜 𝑟𝑒𝑐𝑜𝑛𝑜𝑐𝑖𝑏𝑙𝑒.";
+      throw "Comando no reconocido.";
     }
   } catch (error) {
-    return m.reply(`✖️ *𝐸𝑅𝑅𝑂𝑅_°:* ${error.message}`);
+    return m.reply(`${msm} Ocurrió un error: ${error.message}`);
   }
 };
 
