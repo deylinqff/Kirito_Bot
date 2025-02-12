4707b041257f96d5f658b8ea799754f184974be1
deylinqff <maycraftminecraft4@gmail.com>
2025-02-11 21:08:02 -0600

    Actualizar main-menu.js

--- plugins/main-menu.js
+++ plugins/main-menu.js
@@ -44,6 +44,8 @@ let tags = {
   'advanced': 'ADVANCED',
 }
 
+    let img = 'https://qu.ax/zllFc.jpg' 'https://qu.ax/zXRxo.jpg' 'https://qu.ax/aDkgb.jpg' 'https://qu.ax/VeCrC.jpg'
+
 const defaultMenu = {
   before:  `*⌬━━━━━▣━━◤⌬◢━━▣━━━━━━⌬*
 
@@ -196,7 +198,6 @@ let handler = async (m, { conn, usedPrefix: _p, __dirname }) => {
     let pp13 = 'https://i.ibb.co/Cs6Tt9V/Sylph.jpg'
     let pp14 = 'https://i.ibb.co/JmcS3kv/Sylph.jpg'
     let pp15 = 'https://i.ibb.co/Cs6Tt9V/Sylph.jpg'
-    let img = 'https://i.ibb.co/vvTfkcm/file.jpg'
     await m.react('🚀')
    // await conn.sendMessage(m.chat, { video: { url: [pp, pp2, pp3, pp4, pp5, pp6, pp7, pp8, pp9, pp10, pp11, pp12, pp13, pp14, pp15].getRandom() }, gifPlayback: true, caption: text.trim(), mentions: [m.sender] }, { quoted: estilo })
     await conn.sendFile(m.chat, img, 'thumbnail.jpg', text.trim(), m, null, rcanal)
