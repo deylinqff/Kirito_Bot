fd0c5b3389dc6fe9c495824e55d4279b8527fce5
deylinqff <maycraftminecraft4@gmail.com>
2025-02-13 13:07:27 -0600

    Actualizar rg-perfil.js

--- plugins/rg-perfil.js
+++ plugins/rg-perfil.js
@@ -1,8 +1,20 @@
 import moment from 'moment-timezone';
 import PhoneNumber from 'awesome-phonenumber';
 import fetch from 'node-fetch';
+import fs from 'fs';
+
+const loadMarriages = () => {
+    if (fs.existsSync('./src/database/marry.json')) {
+        const data = JSON.parse(fs.readFileSync('./src/database/marry.json', 'utf-8'));
+        global.db.data.marriages = data;
+    } else {
+        global.db.data.marriages = {};
+    }
+};
 
 let handler = async (m, { conn, args }) => {
+    loadMarriages();
+
     let userId;
     if (m.quoted && m.quoted.sender) {
         userId = m.quoted.sender;
@@ -15,14 +27,19 @@ let handler = async (m, { conn, args }) => {
     let name = conn.getName(userId);
     let cumpleanos = user.birth || 'No especificado';
     let genero = user.genre || 'No especificado';
-    let pareja = user.marry || 'Nadie';
     let description = user.description || 'Sin Descripción';
     let exp = user.exp || 0;
     let nivel = user.level || 0;
-    let role = user.role || 'Sin Rango';
+    let role = user.role || 'Esclavo';
     let coins = user.coin || 0;
     let bankCoins = user.bank || 0;
+
     let perfil = await conn.profilePictureUrl(userId, 'image').catch(_ => 'https://files.catbox.moe/xr2m6u.jpg');
+
+    let isMarried = userId in global.db.data.marriages;
+    let partner = isMarried ? global.db.data.marriages[userId] : null;
+    let partnerName = partner ? conn.getName(partner) : 'Nadie';
+
     let profileText = `
 「👑」 *Perfil* ✰@${userId.split('@')[0]}✰
 ${description}
@@ -46,7 +63,7 @@ ${description}
         contextInfo: {
             mentionedJid: [userId],
             externalAdReply: {
-                title: '✧ Perfil de Usuario ✧',
+                title: '✰ Perfil de Usuario ✰',
                 body: dev,
                 thumbnailUrl: perfil,
                 mediaType: 1,
