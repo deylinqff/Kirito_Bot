f1d861e5ddcedb02668c144b5c571fdb02a33990
deylinqff <maycraftminecraft4@gmail.com>
2025-02-19 13:34:31 -0600

    Actualizar info-creador.js

--- plugins/info-creador.js
+++ plugins/info-creador.js
@@ -1,31 +1,48 @@
+//código creado por Deylin 
+// https://github.com/deylinqff
+// no quites créditos 
+
 import PhoneNumber from 'awesome-phonenumber';
 
-async function handler(m, { conn }) {
-    let numcreador = '50488198573'; // Número del creador
-    let ownerJid = `${numcreador}@s.whatsapp.net`;
+async function handler(m, { conn }) { 
+    let numcreador = '50488198573';
+    let ownerJid = numcreador + '@s.whatsapp.net';
 
-    let name = conn.getName(ownerJid) || 'Deylin';
+    let name = await conn.getName(ownerJid) || 'Deylin'; 
+    let about = (await conn.fetchStatus(ownerJid).catch(() => {}))?.status || 'Sin descripción';
+
+    let empresa = 'Deylin - Servicios Tecnológicos';
 
     let vcard = `
 BEGIN:VCARD
 VERSION:3.0
 N:;${name};;;
 FN:${name}
-TEL;waid=${numcreador}:${PhoneNumber('+' + numcreador).getNumber('international')}
+ORG:${empresa};
+TITLE:CEO & Fundador
+TEL;waid=${numcreador}:${new PhoneNumber('+' + numcreador).getNumber('international')}
+EMAIL:correo@empresa.com
+URL:https://www.tuempresa.com
+NOTE:${about}
+ADR:;;Dirección de tu empresa;;;;
+X-ABADR:ES
+X-ABLabel:Dirección Web
+X-ABLabel:Correo Electrónico
+X-ABLabel:Teléfono de contacto
+X-WA-BIZ-NAME:${name}
+X-WA-BIZ-DESCRIPTION:${about}
 END:VCARD`.trim();
 
     await conn.sendMessage(m.chat, { 
         contacts: { 
             displayName: name, 
-            contacts: [{ vcard, displayName: name }]
+            contacts: [{ vcard }]
         } 
     }, { quoted: m });
 }
 
-handler.help = ['owner'];
-handler.tags = ['main'];
+handler.help = ['owner']; 
+handler.tags = ['main']; 
 handler.command = ['owner', 'creator', 'creador', 'dueño'];
 
 export default handler;
-
-// Código creado por Deyin
