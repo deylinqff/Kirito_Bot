ec8ee1dba6f7ae59a8584a151a5818e2ce3490a8
deylinqff <maycraftminecraft4@gmail.com>
2025-02-20 12:34:24 -0600

    Actualizar jadibot-serbot.js

--- plugins/jadibot-serbot.js
+++ plugins/jadibot-serbot.js
@@ -1,6 +1,5 @@
 import { useMultiFileAuthState, makeWASocket, fetchLatestBaileysVersion, jidNormalizedUser } from '@whiskeysockets/baileys';
 import pino from 'pino';
-import readline from 'readline';
 import fs from 'fs';
 import NodeCache from 'node-cache';
 import { makeCacheableSignalKeyStore } from '@whiskeysockets/baileys';
@@ -9,27 +8,30 @@ import { DisconnectReason, MessageRetryMap } from '@whiskeysockets/baileys';
 if (!(global.conns instanceof Array)) global.conns = [];
 
 let handler = async (m, { conn: _conn, args, usedPrefix, command, isOwner }) => {
-  const bot = global.db.data.settings[conn.user.jid] || {};
-  
-  if (!bot.jadibotmd) return m.reply('⚠️ **¡Comando Desactivado!** Este comando está desactivado por mi creador.');
+  const bot = global.db?.data?.settings[conn.user.jid] || {};
+
+  if (!bot?.jadibotmd) return m.reply('⚠️ **¡Comando Desactivado!** Este comando está desactivado por mi creador.');
 
   let parent = args[0] && args[0] == 'plz' ? _conn : await global.conn;
 
-  // Función que maneja la conexión del bot
   async function serbot() {
-    let authFolderB = m.sender.split('@')[0];
+    const authFolderB = m.sender.split('@')[0];
     const userFolderPath = `./CrowKiritoBot/${authFolderB}`;
 
     if (!fs.existsSync(userFolderPath)) {
       fs.mkdirSync(userFolderPath, { recursive: true });
     }
 
-    if (args[0]) fs.writeFileSync(`${userFolderPath}/creds.json`, JSON.stringify(JSON.parse(Buffer.from(args[0], "base64").toString("utf-8")), null, '\t'));
+    if (args[0]) {
+      const creds = Buffer.from(args[0], 'base64').toString('utf-8');
+      fs.writeFileSync(`${userFolderPath}/creds.json`, creds);
+    }
 
     const { state, saveState, saveCreds } = await useMultiFileAuthState(userFolderPath);
     const msgRetryCounterMap = MessageRetryMap;
     const msgRetryCounterCache = new NodeCache();
     const { version } = await fetchLatestBaileysVersion();
+
     let phoneNumber = m.sender.split('@')[0];
 
     const connectionOptions = {
@@ -43,11 +45,6 @@ let handler = async (m, { conn: _conn, args, usedPrefix, command, isOwner }) => 
       },
       markOnlineOnConnect: true,
       generateHighQualityLinkPreview: true,
-      getMessage: async (clave) => {
-        let jid = jidNormalizedUser(clave.remoteJid);
-        let msg = await store.loadMessage(jid, clave.id);
-        return msg?.message || "";
-      },
       msgRetryCounterCache,
       msgRetryCounterMap,
       version
@@ -58,25 +55,24 @@ let handler = async (m, { conn: _conn, args, usedPrefix, command, isOwner }) => 
     if (args[0] && !conn.authState.creds.registered) {
       if (!phoneNumber) process.exit(0);
       let cleanedNumber = phoneNumber.replace(/[^0-9]/g, '');
+
       setTimeout(async () => {
         let codeBot = await conn.requestPairingCode(cleanedNumber);
         codeBot = codeBot?.match(/.{1,4}/g)?.join("-") || codeBot;
-        let txt = `*¡Hola, ${m.sender.split('@')[0]}! Aquí está tu código para activar el Kirito-Bot!*\n\n`
-        txt += `🎯 *Pasos:*\n`
-        txt += `1️⃣ Abre WhatsApp y ve a la opción de *"Dispositivos Vinculados"*.\n`
-        txt += `2️⃣ Toca la opción *"Vincular con número"*.\n`
-        txt += `3️⃣ Introduce el siguiente código en tu WhatsApp:\n>`
-        txt += `✨ *Recuerda:* Este código solo es válido para el número registrado.\n`
+        let txt = `*¡Hola, ${m.sender.split('@')[0]}! Aquí está tu código para activar el Kirito-Bot!*\n\n`;
+        txt += `🎯 *Pasos:*\n`;
+        txt += `1️⃣ Abre WhatsApp y ve a la opción de *"Dispositivos Vinculados"*.\n`;
+        txt += `2️⃣ Toca la opción *"Vincular con número"*.\n`;
+        txt += `3️⃣ Introduce el siguiente código en tu WhatsApp:\n`;
+        txt += `✨ *Recuerda:* Este código solo es válido para el número registrado.\n`;
         await parent.reply(m.chat, txt, m);
         await parent.reply(m.chat, codeBot, m);
       }, 3000);
     }
 
     conn.isInit = false;
-
     let isInit = true;
 
-    // Actualización de conexión
     async function connectionUpdate(update) {
       const { connection, lastDisconnect, isNewLogin, qr } = update;
       if (isNewLogin) conn.isInit = true;
@@ -99,7 +95,6 @@ let handler = async (m, { conn: _conn, args, usedPrefix, command, isOwner }) => 
       }
     }, 60000);
 
-    // Recargar el handler
     let creloadHandler = async function (restatConn) {
       try {
         const Handler = await import(`../handler.js?update=${Date.now()}`).catch(console.error);
