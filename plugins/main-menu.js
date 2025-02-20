bb00bb1aaeccd114d2b791c372b02d67bb1a3c72
deylinqff <maycraftminecraft4@gmail.com>
2025-02-20 08:29:21 -0600

    Actualizar main-menu.js

--- plugins/main-menu.js
+++ plugins/main-menu.js
@@ -71,6 +71,7 @@ const formatoMenu = {
   despues: '🔥 *By DEYLIN* 🔥',
 };
 
+
 const more = String.fromCharCode(8206);
 const readMore = more.repeat(4001);
 
@@ -137,35 +138,7 @@ const handler = async (m, { conn, usedPrefix }) => {
 
     const imagenAleatoria = imagenesURL[Math.floor(Math.random() * imagenesURL.length)];
 
-    const txt = menuTexto.trim();
-    const userId = m.sender;
-    const botname = 'Kirito-Bot';
-    const textbot = 'Un bot para ayudarte en WhatsApp';
-    const banner = imagenAleatoria;
-    const channelRD = { id: '120363147505323988@g.us', name: 'Canal Oficial de Kirito-Bot' };
-
-    await conn.sendMessage(m.chat, { 
-      text: txt,
-      contextInfo: {
-        mentionedJid: [m.sender, userId],
-        isForwarded: true,
-        forwardedNewsletterMessageInfo: {
-          newsletterJid: channelRD.id,
-          newsletterName: channelRD.name,
-          serverMessageId: -1,
-        },
-        forwardingScore: 999,
-        externalAdReply: {
-          title: botname,
-          body: textbot,
-          thumbnailUrl: banner,
-          mediaType: 1,
-          showAdAttribution: true,
-          renderLargerThumbnail: true,
-        },
-      },
-    }, { quoted: m });
-
+    await conn.sendFile(m.chat, imagenAleatoria, 'menu.jpg', menuTexto.trim(), m);
   } catch (error) {
     console.error('Error en el menú:', error);
     conn.reply(m.chat, '❌ Error al generar el menú.', m);
