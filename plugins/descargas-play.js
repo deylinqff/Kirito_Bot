da6a80a4cc7e1d2097fbf50ab1060e3657f118ad
deylinqff <maycraftminecraft4@gmail.com>
2025-02-20 14:14:43 -0600

    Actualizar descargas-play.js

--- plugins/descargas-play.js
+++ plugins/descargas-play.js
@@ -145,7 +145,7 @@ const handler = async (m, { conn, text, usedPrefix, command }) => {
   }
 };
 
-handler.command = handler.help = ["play", "play2", "ytmp3", "yta", "ytmp4", "ytv"];
+handler.command = handler.help = ["pla", "pla", "ytmp3", "yta", "ytmp4", "ytv"];
 handler.tags = ["downloader"];
 
 export default handler;
